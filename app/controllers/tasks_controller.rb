71a79755d7422cf0753f8b61892791d316155f8d
EC2 Default User <ec2-user@ip-172-31-11-233.ap-northeast-1.compute.internal>
2022-10-05 01:49:11 +0000

    【C6.3】タスクの新規作成ページを作り、機能を実装する（ＭＶＣ）３
    ＜だいぶ時間を食ったけど、ようやくcreateアクションの実装！
    　＆その後はrender使ってエラーメッセージ表示（こっちはまた今度）！＞
    
    （※まずは【勤怠チュート5.5.2】を見ながらcreateアクション記述やな！
    　　今更だけど・・・『POSTリクエストがフォーム送信を受け取る』って書いとるわね。
    　　だからform_withヘルパーメソッドの送信先は
    　　やっぱPOSTリクエストの先＝createアクションのパスって事なんやろな。）
    
    （※あと江草さん曰く、
    　　before_action（とStrong Parameters）を設定するまでは
    　　コントローラー内の全アクション記述に
    　　「@user = User.find(params[:user_id])」が必要ゆ～てたな。
    　　じゃないと、@taskだけ定義してもその引用元が分からんもんね。）
    
    （※んで、【勤怠チュート5.5.2】では
    　　「@user = User.new（params[:user]）」で
    　　createアクション用の@userを定義しとるが、今回は@taskを定義せにゃだから・・・
    　　「@task = Task.new（params[:task]）」とでも書いとくかな。）
    
    １：（controllers/tasks_controller.rb）内の
    
    ・１２行目に空白行を追加。
    ・１３行目に「def create」を新記述（テキストまね）。
    ・１４行目に「@user = User.find(params[:user_id])」を新記述。
    ・１５行目に「@task = Task.new（params[:task]）」を新記述（テまね）。
    ・１６行目に「if @task.save」を新記述（テまね）。
    ・１７行目に「＃ 保存成功の場合、ここに記述した処理が実行される。」を新記述（テまね）。
    ・１８行目に「else」を新記述（テまね）。
    ・１９行目に「render :new」を新記述（テまね）；。
    ・２０行目に「end」を新記述してif～else文を〆。
    ・２１行目に「end」を新記述してcreateアクション記述全体を〆。
    
    （※・・・ひとまず、こんな感じか？
    　　【勤怠チュート5.5.2】的には、保存成功時の既述がまだだから
    　　当然このままだとエラーが出るとして・・・
    　　次の【勤怠チュート5.5.3】で、いよいよStrong Parameters設定か・・・。
    
    　　とりま、出勤せにゃならんし、一旦ここまでにしとくか！）
    
    ＜※【B8】以降でヤる予定リスト※＞
    ・予定１：【views/users/_form.html.erb】作成＆余計なテキスト削除・・・未！
    ・予定２：エラーメッセージを見本通りに合わせる＆重複文削除・・・未！
    ・予定３：params.require使って、task_paramsを定義・・・未！
    ・予定４：「'＃'」のままのパス系を今度ちゃんと書く・・・未！
    
    （以上な現状！）

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,4 +9,14 @@ class TasksController < ApplicationController
     @user = User.find(params[:user_id])
     @task = Task.new
   end
+  
+  def create
+    @user = User.find(params[:user_id])
+    @task = Task.new(params[:task])
+    if @task.save
+      # 保存成功の場合、ここに記述した処理が実行される。
+    else
+      render :new
+    end
+  end
 end
